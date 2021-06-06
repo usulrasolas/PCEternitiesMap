9933e129a7293cd777ce68ae032f8fa6c82a3a73
usulrasolas <lhs.one@gmail.com>
2021-06-06 11:13:46 -0500

    splashpage clickthrough

--- EternitiesMap/My Project/Application.Designer.vb
+++ EternitiesMap/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.EternitiesMap.MainMenu
+            Me.MainForm = Global.EternitiesMap.SplashPage
         End Sub
     End Class
 End Namespace
--- EternitiesMap/SplashPage.vb
+++ EternitiesMap/SplashPage.vb
@@ -1,5 +1,10 @@
 ﻿Public Class SplashPage
     Private Sub BCloseSplash_Click(sender As Object, e As EventArgs) Handles BCloseSplash.Click
+        Dim oForm As Form
+#Disable Warning CA2000 ' Dispose objects before losing scope
+        oForm = New MainMenu
+#Enable Warning CA2000 ' Dispose objects before losing scope
+        oForm.Show()
         Close()
     End Sub
 
